8fbaeb1acd9b5f3c38b3ff6124778a0cb96a81cc
magicsuperpanda <1583224387@qq.com>
2018-12-16 10:55:42 +0800

    为了统计学生人数，可以给Student类增加一个类属性，每创建一个实例，数量自动加1

--- PY/allTest/test2.py
+++ PY/allTest/test2.py
@@ -1,5 +1,20 @@
-def f(x):
-  return x * x
+class Student(object):
+  count=0
 
+  def __init__(self, name):
+    self.name = name
+    Student.count += 1
 
-print(f(5))
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
