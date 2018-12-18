21e6d79505c993d0bcdc4773d04b421fd7420086
magicsuperpanda <1583224387@qq.com>
2018-12-18 19:59:07 +0800

    file

--- PY/allTest/test2.py
+++ PY/allTest/test2.py
@@ -1,9 +1,3 @@
-try:
-    print('try...')
-    r = 10 / 0
-    print('result:', r)
-except ZeroDivisionError as e:
-    print('except:', e)
-finally:
-    print('finally...')
-print('END')
+f=open('D:/test/test.txt', 'a')
+f.write("\n hello,test4" )
+f.close()
